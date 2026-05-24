69c15d8c7f28561cae9a536f8f92ad9d8a1ed733
boshilin <784010563bo@gmail.com>
2026-05-24 11:36:03 +0800

    feat: 更新开发环境配置

--- backend/config.py
+++ backend/config.py
@@ -5,11 +5,11 @@ class Config:
     APP_ENV = os.getenv("APP_ENV", "development")
     SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
 
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "jushi-mysql")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
     MYSQL_PORT = int(os.getenv("MYSQL_PORT", "3306"))
     MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "jushi")
-    MYSQL_USER = os.getenv("MYSQL_USER", "jushi")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "jushi_pass")
+    MYSQL_USER = os.getenv("MYSQL_USER", "root")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "504803")
 
     DCE_API_BASE = os.getenv("DCE_API_BASE", "")
     DCE_CLUSTER = os.getenv("DCE_CLUSTER", "default")
